8624a67c3776c2cb230c2eea1500a5f1a6a32b1f
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2024-02-12 22:44:59 +0100

    created the 4 basics operations method in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String Operation;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an operation : multiplication, addition, division and power. \n");
+        Operation = scanner.next();
+        Operation = Operation.toUpperCase();
+        switch (Operation) {
+            case "MULTIPLICATION":
+                multiplication();
+                break;
+            case "ADDITION":
+                addition();
+                break;
+            case "DIVISION":
+                division();
+                break;
+            case "POWER":
+                power();
+            default:
+                System.out.println("The operation has not been written correclty\n");
+        }
+        scanner.close();
     }
+    public static void multiplication(){
+        Scanner scanner = new Scanner(System.in);
+        float x,y,result;
+        System.out.println("Give the first float");
+        x=scanner.nextFloat();
+        System.out.println("Give the second float");
+        y=scanner.nextFloat();
+        result=x*y;
+        System.out.println(result);
+        scanner.close();
+    }
+    public static void addition(){
+        Scanner scanner = new Scanner(System.in);
+        float x,y,result;
+        System.out.println("Give the first float");
+        x=scanner.nextFloat();
+        System.out.println("Give the second float");
+        y=scanner.nextFloat();
+        result=x+y;
+        System.out.println(result);
+        scanner.close();
+    }
+    public static void division(){
+        Scanner scanner = new Scanner(System.in);
+        float x,y,result;
+        System.out.println("Give the first float");
+        x=scanner.nextFloat();
+        System.out.println("Give the second float");
+        y=scanner.nextFloat();
+        if (y!=0){
+            result=x/y;
+            System.out.println(result);
+        }
+        else{System.out.println("Error:Division by zero");}
+        scanner.close();
+    }
+    public static void power(){
+        Scanner scanner = new Scanner(System.in);
+        int x,y;
+        double result;
+        System.out.println("Give the first integer");
+        x=scanner.nextInt();
+        System.out.println("Give the second integer");
+        y=scanner.nextInt();
+        result=pow(x,y);
+        System.out.println(result);
+        scanner.close();
+    }
+
 }
