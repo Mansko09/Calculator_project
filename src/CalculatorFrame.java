f2c25bde9c5fa815715b762b2953e1d70fb7b226
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2024-02-13 17:04:18 +0100

    added a Jlabel and changed background

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -1,24 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class CalculatorGUI extends JFrame {
-    CalculatorGUI() {
+public class CalculatorFrame extends JFrame {
+    CalculatorFrame() {
+        ImageIcon iconlogo = new ImageIcon("C:\\Users\\mbeng\\Documents\\Programming_projects\\java_games\\Calculator_project\\calculator-icon.png");
+        this.setIconImage(iconlogo.getImage());
+
         JLabel label = new JLabel();
-        label.setText(" Pink Calculator");
-        this.setTitle("Calculator by Mansko09");
+        //label.setText(" Pink Calculator ");
+
+        ImageIcon image = new ImageIcon("C:\\Users\\mbeng\\Documents\\Programming_projects\\java_games\\Calculator_project\\CALCULATOR.png");
+        label.setIcon(image);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        this.setTitle("The pink Calculator ");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of application
         this.setResizable(false);//prevent this from being resized
-        this.setSize(400, 600);
+        this.setSize(300, 500);
         this.setVisible(true); //make this visible
+        this.getContentPane().setBackground(Color.pink);
 
-        try{
-            ImageIcon image = new ImageIcon("C:\\Users\\mbeng\\Documents\\Programming_projects\\java_games\\Calculator_project\\lib\\Calculator_logo.webp");
-            this.setIconImage(image.getImage());//change Icon of this
-        }catch(Exception e){
-            e.printStackTrace();
-            System.out.println("The file path cannot be found");
-        }
-        this.getContentPane().setBackground(Color.white);
 
         this.add(label);
     }
